d7bb6416081b6f107ae1d9482c30e5a7b6b59e0f
ElperdonXV <davidemartinografico@gmail.com>
2022-01-26 16:11:49 +0100

    done nav circle

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="app">
+  <div class="relative" id="app">
     <Header />
     <Main />
     <Footer />
+    <a href="#" class="nav-circle">
+      <i class="fas fa-arrow-up"></i>
+    </a>
   </div>
 </template>
 
@@ -23,9 +26,31 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+@import "./assets/scss/partials/_variables.scss";
 @import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@300;400;500;600;700&family=PT+Sans+Narrow:wght@400;700&display=swap');
+.relative{
+  position: relative;
+}
 #app{
   max-width: 100%;
   overflow: hidden;
+  .nav-circle{
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    width: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    border-radius: 50%;
+    background-color: $mountain;
+    padding: 10px;
+    i{
+      color: white;
+      font-size: 2em;
+    }
+  }
 }
 </style>
